59931b1d8f91d7b65a12a351d7763dbb24bffcd2
sgchris@gmail.com <sgchris@gmail.com>
2014-02-19 09:18:38 +0200

    updated RESTFul example. Updated initial file

--- lib/Testify.php
+++ lib/Testify.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * execute one or more tests at once.
+ * with all the initializations
+ *
+ * format
+ * 		php Testify.php some/test.php another/test.php
+ *
+ * @author Gregory Chris <sgchris@gmail.com>
+ * @TODO gather all the result to one stack and display total results
+ */
 
 // check input parameters
 if ($_SERVER['argc'] < 2) {
@@ -21,6 +31,7 @@ if (!empty($tests)) {
         // remove the extension
         $test = preg_replace('%\.php$%i', '', $test);
 
+		// include the file
         if (file_exists(__DIR__.'/'.$test.'.php')) {
             require __DIR__.'/'.$test.'.php';
         } else {
